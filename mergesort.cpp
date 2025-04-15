104c720cad61f2d16f9d0992d593127148d73a9a
sheilloputratama <sheilloggendut@gmail.com>
2025-04-15 10:58:33 +0700

    membuat struktur dasar fungsi merge sort

--- mergesort.cpp
+++ mergesort.cpp
@@ -18,3 +18,17 @@ void input() {
         cin >> arr[i];
     }
 }
+
+// Create function to merge two sorted arrays
+void mergesort(int low, int high) { // Step 1
+    if (low >= high) {
+        return; // Step 1.a
+    }
+
+    int mid = (low + high) / 2; // Step 2
+
+    // Step 3
+    mergesort(low, mid); // Step 3.a
+    mergesort(mid + 1, high); // Step 3.b
+
+    
