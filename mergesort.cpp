bb779cb99eb5663d96c87e2c1fed3c22e254650e
sheilloputratama <sheilloggendut@gmail.com>
2025-04-15 10:55:02 +0700

    menambahkan validasi input jumlah element

--- mergesort.cpp
+++ mergesort.cpp
@@ -4,3 +4,13 @@ using namespace std;
 // Create main and temporary array
 int arr[20], B[20];
 int n;
+
+void input() {
+    while (true) {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if (n <= 20) break;
+        else cout << "Please enter a number less than or equal to 20." << endl;
+    }
+
+    
